335d38c7f9aad671609f334405c494bd4d1192ed
Wu GuoLin <wuguolin@cvnavi.com>
2018-10-25 18:16:49 +0800

    add file read/write

--- src/main/java/com/mispower/utils/file/FileAdaptor.java
+++ src/main/java/com/mispower/utils/file/FileAdaptor.java
@@ -1,8 +1,10 @@
 package com.mispower.utils.file;
 
-import com.sun.istack.internal.NotNull;
+import com.google.common.base.Preconditions;
 
-import java.io.File;
+import java.io.*;
+import java.util.LinkedList;
+import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * FileAdaptor provides read and write access to a variety of files.
@@ -11,54 +13,187 @@ import java.io.File;
  * @author wuguolin
  */
 public class FileAdaptor {
+
+    /**
+     * input file
+     */
+    private final File file;
+    /**
+     * max buffer size for every read,used for read big file.
+     */
+    private final int maxReadBuffer;
+    /**
+     * output directory. If empty,will not write output
+     */
+    private final String outputDir;
+    /**
+     * file suffix.To consistency,write output file will be remain same suffix as input file.
+     */
     private String suffix;
-    private String sourcePath;
-    private String targetPath;
-    private String fileName;
+    /**
+     * file name without suffix
+     */
+    private String fileNameWithoutSuf;
 
-    private byte[] buffer;
+    private final int fileHasCode;
 
-    private FileAdaptor(String sourcePath,  String targetPath) {
-        this.sourcePath = sourcePath;
-        this.targetPath = targetPath;
+    /**
+     * read buffer
+     */
+    private final byte[] buffer;
+
+
+    private final LinkedList<byte[]> dataQueue = new LinkedList<>();
+
+//    private final AtomicInteger maxBufferSize = new AtomicInteger(1024);
+//
+//    private final AtomicInteger maxBufferSize = new AtomicInteger(1000);
+
+    /**
+     * dot of file suffix
+     */
+    private final String SEPARATOR = ".";
+
+    private FileAdaptor(File file, int maxReadBuffer, String outputDir) {
+        this.file = file;
+        this.fileHasCode = file.hashCode();
+        this.maxReadBuffer = maxReadBuffer;
+        this.outputDir = outputDir;
+        buffer = new byte[maxReadBuffer];
+        init();
     }
 
-    public byte[] read() {
-        File file = new File(sourcePath);
-        if (file.isFile()) {
-            fileName = file.getName();
+    private void init() {
+        String fileName = file.getName();
+        int dot = fileName.lastIndexOf(SEPARATOR);
+        suffix = fileName.substring(dot + 1);
+        fileNameWithoutSuf = fileName.substring(0, dot);
 
+    }
+
+    /**
+     * read file to buffer stream
+     *
+     * @return
+     */
+    public int read() {
+        try {
+            Long start = System.currentTimeMillis();
+            FileInputStream fileInputStream = new FileInputStream(file);
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+            int len = 0;
+            byte[] copy;
+            while ((len=bufferedInputStream.read(buffer)) != -1) {
+                copy=new byte[len];
+                System.arraycopy(buffer,0,copy,0,len);
+                offerQueue(copy);
+
+            }
+            Long end = System.currentTimeMillis();
+            System.out.println("use time:" + maxReadBuffer + ": leap size:" + ":" + (end - start));
+            bufferedInputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+
+    public void offerQueue(byte[] data) {
+        if (data != null) {
+            synchronized (dataQueue) {
+                dataQueue.offer(data);
+            }
+        }
+    }
+
+    public void doProcess() {
+        write();
+
+    }
+
+    private void write() {
+        File writeFile = preWrite();
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(writeFile, true);
+            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+            final LinkedBlockingQueue<byte[]> tmp = new LinkedBlockingQueue<>();
+            synchronized (dataQueue) {
+                tmp.addAll(dataQueue);
+                dataQueue.clear();
+            }
+            for (byte[] data : tmp) {
+                bufferedOutputStream.write(data, 0, data.length);
+            }
+            bufferedOutputStream.flush();
+            bufferedOutputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        return null;
-    }
-
-    public void write() {
 
     }
+
+    private File preWrite() {
+        Preconditions.checkNotNull(outputDir, "outputDir must be not empty if want to write");
+        final File outDir = new File(outputDir);
+        if (!outDir.exists()) {
+            outDir.mkdir();
+        }
+        Preconditions.checkState(outDir.isDirectory(),
+                "Path is not a directory: " + outDir.getAbsolutePath());
+        StringBuffer sb = new StringBuffer();
+        sb.append(outputDir);
+        sb.append("/");
+        sb.append(fileHasCode);
+        sb.append(SEPARATOR);
+        sb.append(suffix);
+        return new File(sb.toString());
+    }
+
 
     public String getSuffix() {
-        int dot = sourcePath.lastIndexOf(".");
-        return sourcePath.substring(dot + 1);
+        return suffix;
     }
 
+    public String getFileName() {
+        return fileNameWithoutSuf;
+    }
+
+    /**
+     * Builder Pattern
+     */
     public static class Builder {
-        private String sourcePath;
-        private String targetPath;
+        private String inputFile;
+        private int maxReadBuffer = 1024;
+        private String outputDir;
 
-        public Builder setTargetPath(String targetPath) {
-            this.targetPath = targetPath;
+        public Builder setInputFile(String inputFile) {
+            this.inputFile = inputFile;
             return this;
         }
 
-        public Builder setSourcePath(String sourcePath) {
-            this.sourcePath = sourcePath;
+        public Builder setMaxReadBuffer(int maxReadBuffer) {
+            this.maxReadBuffer = maxReadBuffer;
             return this;
         }
+
+        public Builder setOutputDir(String outputDir) {
+            this.outputDir = outputDir;
+            return this;
+        }
+
 
         public FileAdaptor build() {
-
-            return new FileAdaptor(sourcePath, null);
+            // Sanity checks
+            Preconditions.checkNotNull(inputFile, "must be specify input path");
+            File file = new File(inputFile);
+            Preconditions.checkState(file.isFile(), "The parameter type must be a file");
+            return new FileAdaptor(file, maxReadBuffer, outputDir);
         }
     }
 
--- src/test/java/test.java
+++ src/test/java/test.java
@@ -5,15 +5,20 @@ public class test {
 
     @Test
     public void testFileAdaptor() {
-        FileAdaptor fileAdaptor = new FileAdaptor.Builder().setSourcePath("D:/1.txt").build();
-        System.out.println(fileAdaptor.getSuffix());
+        FileAdaptor fileAdaptor = new FileAdaptor.Builder().setInputFile("D:/1.mp4").setOutputDir("D:/1").
+               build();
+        fileAdaptor.read();
+        fileAdaptor.doProcess();
+
+
+        //System.out.println(fileAdaptor.getSuffix());
     }
 
     @Test
-    public void testAssert(){
-        assert 1==1;
+    public void testAssert() {
+        assert 1 == 1;
         System.out.println(1);
-        assert 1==2:"类型不正确";
+        assert 1 == 2 : "类型不正确";
         System.out.println("1==2");
     }
 }
